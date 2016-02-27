470090a0f43ac7616e595a0f4d31df5cecdabd27
T-Parker <t.a.parker8891@gmail.com>
2016-02-26 22:21:27 -0500

    Added files via upload
    My first c++ lesson.

---
+++ My first c++ project/main.cpp
@@ -0,0 +1,14 @@
+//This is my first comment!
+//Author: Tiffany
+//version: 1.0   completion date: 2/26/2016
+
+
+#include <iostream> //puts the iostream package into the code.
+
+using namespace std; //lets your code access the standard c++ library.
+
+int main() //your main function.
+{ //the main function's open bracket.
+    cout << "Hello world!" << endl; //a command that displays "Hello world!".
+    return 0; //statement end, ends the program.
+} //the main function's close bracket.
